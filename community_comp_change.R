7a8995e5b333ebc3fc60e33cdbee489fd0e947f0
Leah Veldhuisen <77695333+lveldhuisen@users.noreply.github.com>
2024-08-27 16:15:05 -0700

    update

---
+++ community_comp_change.R
@@ -0,0 +1,21 @@
+#data wrangling and reformatting with Lorah's transplant data
+#pivot wider to make comm phylo matrix 
+
+install.packages("lme4") #modelling
+install.packages("lmerTest") #provides p values
+install.packages("performance") #model diagnostics
+install.packages("ggeffects") #easy prediction & comparison
+
+library(tidyverse)
+library(lme4)
+library(lmerTest)
+library(performance)
+library(ggeffects)
+
+#bring in data
+abundance_df <- read.csv("occurance2017-2023.csv")
+
+#test model 
+hist(abundance_df$occurrenceCount)
+
+lmer(occurrenceCount ~ Year * treatment + originSite +(1||Species))
--- data_reformatting.R
+++
@@ -1,6 +0,0 @@
-#data wrangling and reformatting with Lorah's transplant data
-
-library(tidyverse)
-
-#bring in data
-intensivecover_df <- read.csv("Final_IntensiveCover_2018-2023_RMBLTransplant.csv")
