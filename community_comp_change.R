f18d9c9c6d5c1195202d7733c16de3854166e614
Leah Veldhuisen <77695333+lveldhuisen@users.noreply.github.com>
2024-08-28 14:23:25 -0700

    Update community_comp_change.R

--- community_comp_change.R
+++ community_comp_change.R
@@ -5,12 +5,14 @@ install.packages("lme4") #modelling
 install.packages("lmerTest") #provides p values
 install.packages("performance") #model diagnostics
 install.packages("ggeffects") #easy prediction & comparison
+install.packages("sjPlot") #visualize random effects
 
 library(tidyverse)
 library(lme4)
 library(lmerTest)
 library(performance)
 library(ggeffects)
+library(sjPlot)
 
 #bring in data
 abundance_df <- read.csv("occurance2017-2023.csv")
@@ -18,6 +20,17 @@ abundance_df <- read.csv("occurance2017-2023.csv")
 #test model 
 hist(abundance_df$occurrenceCount)
 
-test <- lmer(occurrenceCount ~ year * treatment + originSite +(1|species),
+model1 <- lmer(occurrenceCount ~ year * treatment + originSite + (1|species),
      data = abundance_df)
-summary(test)
+summary(model1)
+
+(re.effects <- plot_model(model1, type = "re", show.values = TRUE))
+plot(re.effects)
+
+ggplot(abundance_df, aes(x = year, y = occurenceCount, colour = treatment)) +
+  facet_wrap(~originSite, nrow=2) +   # a panel for each mountain range
+  geom_point(alpha = 0.5) +
+  theme_classic() +
+  geom_line(data = cbind(abundance_df, pred = predict(model1)), aes(y = pred), size = 1) +  # adding predicted line from mixed model 
+  theme(legend.position = "none",
+        panel.spacing = unit(2, "lines"))  
