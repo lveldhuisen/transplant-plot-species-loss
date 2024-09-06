8295867ec5bfb1c201859d92fc0ffdd95d5a1dfb
Leah Veldhuisen <77695333+lveldhuisen@users.noreply.github.com>
2024-09-06 10:10:31 -0700

    Update comm_phylo_analyses.R

--- comm_phylo_analyses.R
+++ comm_phylo_analyses.R
@@ -12,8 +12,8 @@ library(janitor)
 library(patchwork)
 library(car)
 
-#import S&B phylogeny-----------------------------------------------------------
-setwd("~/Library/CloudStorage/OneDrive-UniversityofArizona/Arizona PhD/Research/RMBL phylogeny/Smith&Brown18")
+#import & prune S&B phylogeny-------------------------------------------------
+setwd("~/Library/CloudStorage/OneDrive-UniversityofArizona/Arizona PhD/Research/RMBL phylogeny/Smith&Brown18") #phylogeny not stored in R project
 
 ##import S&B18 tree and check data## 
 SBtree <- read.tree(file = "ALLMB.tre")
@@ -24,13 +24,81 @@ is.rooted(SBtree)
 #bring in raw data
 abundance_df <- read.csv("occurance2017-2023.csv")
 
-# remove non-species from species column
+#remove non-species from species column
 gc.outs <- c("litter", "bare_soil", "rock")
 
 #filter data for things you never want
 abundance_forphylogeny <- abundance_df %>% filter(!is.na(treatment),
                                          !species %in% gc.outs)
 
+#reformat data to be community data matrix
+matrix_forphylogeny <- pivot_wider(abundance_forphylogeny, names_from = species, 
+                                    values_from = occurrenceCount)
+#remove extra columns
+matrix_forphylogeny = subset(matrix_forphylogeny, select = -c(
+  turfID, originSite, destinationSite,originPlotID,date_yyyymmdd,
+  treatment,functionalGroup, unknownMorpho, year, percentCover, X, X.1) )
+
+#replace NAs with 0s
+matrix_forphylogeny[is.na(matrix_forphylogeny)] <- 0
+
+#switch treatments to row names for the matrix 
+matrix_forphylogeny <- matrix_forphylogeny %>%
+  group_by(treatmentOriginGroup) %>%
+  summarise_if(
+    is.numeric,
+    sum,
+    na.rm = TRUE
+  )
+#make row names
+matrix_forphylogeny <- matrix_forphylogeny %>% column_to_rownames(var = "treatmentOriginGroup")
+
+#sum columns to have column with all species for phylogeny
+species_sums <- colSums(matrix_forphylogeny)
+
+# Convert column sums to a data frame row
+sum_row <- as.data.frame(t(species_sums))
+names(sum_row) <- names(matrix_forphylogeny)  # Ensure column names match
+
+rownames(sum_row) <- c("all") 
+
+#add sum df to community matrix
+matrix_forphylogeny <- rbind(matrix_forphylogeny, sum_row)
+
+#get rid of unknowns 
+matrix_forphylogeny = subset(matrix_forphylogeny, select = -c(moss, unknown_forb,
+                                                              Unknown_round_leaves,
+                                                              unknown_seedling))
+
+#substitute species names that aren't in phylogeny (see replacement list in Google Drive)
+matrix_forphylogeny <- matrix_forphylogeny %>% 
+  rename(
+    Agoseris_heterophylla = Agoseris_sp.,
+    Agoseris_glauca_var._dasycephala = Agoseris_glauca,
+    Orobanche_fasciculata = Aphyllon_fasciculatum,
+    Carex_nelsonii = Carex_sp.,
+    Chamerion_angustifolium = Chamaenerion_angustifolium,
+    Epilobium_ciliatum = Epilobium_sp.,
+    Erigeron_pinnatisectus = Erigeron_coulteri,
+    Erigeron_grandiflorus = Erigeron_elatior,
+    Erigeron_compositus = Erigeron_sp., 
+    Festuca_rubra_subsp._rubra = Festuca_rubra,
+    Helianthella_uniflora = Helianthella_quinquenervis,
+    Heterotheca_villosa = Heterotheca_pumila,
+    Hydrophyllum_capitatum_var._capitatum = Hydrophyllum_capitatum,
+    Lupinus_argenteus = Lupinus_sp.,
+    Poa_pratensis_subsp._pratensis = Poa_pratensis,
+    Polygonum_douglasii_subsp._douglasii = Polygonum_douglasii,
+    Rhodiola_integrifolia = Sedum_integrifolium,
+    Senecio_triangularis = Senecio_integerrimus,
+    Senecio_serra = Senecio_sp., 
+    Achnatherum_nelsonii = Stipa_nelsonii, 
+    Symphyotrichum_foliaceum = Symphyotrichum_ascendens, 
+    Bouteloua_gracilis = unknown_grass,
+    Veratrum_virginicum = Veratrum_californicum
+  )
+
+
 #prune tree
-pruned.tree <- treedata(SBtree, unlist(community_matrix[4,community_matrix[4,]>0]), warnings = F)$phy
+pruned.tree <- treedata(SBtree, unlist(matrix_forphylogeny[10,matrix_forphylogeny[10,]>0]), warnings = F)$phy
 plot(pruned.tree)
