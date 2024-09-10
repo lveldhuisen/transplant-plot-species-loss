c2ff170850d911c13d9c814fcf0379a3bb8a1432
Leah Veldhuisen <77695333+lveldhuisen@users.noreply.github.com>
2024-09-10 11:11:43 -0700

    update

--- 01_DataPrep/Data_cleaning_community_diversity.R
+++ 01_DataPrep/Data_cleaning_community_diversity.R
@@ -1,11 +1,12 @@
 library(dplyr)
 library(tidyverse)
 
-#data cleaning for community diversity metrics
-
 #Data cleaning and formatting---------------------------------------------------
+
+##all abundance data reformatting############
+
 #bring in data
-abundance_df <- read.csv("occurance2017-2023.csv")
+abundance_df <- read.csv("Data/occurance2017-2023.csv")
 abundance_df$year <- as.factor(abundance_df$year)
 
 #use only these years
@@ -13,7 +14,6 @@ ins <- c("2018","2019","2021","2022","2023")
 
 #get rid of extra control plots
 outs <- c("untouched","within_site_transplant")
-ins_controlonly <- c("netted_untouched","within_site_transplant","untouched" )
 
 # remove non-species from species column
 gc.outs <- c("litter", "bare_soil", "rock")
@@ -29,3 +29,87 @@ abundance_df1 <- abundance_df %>% filter(!is.na(treatment),
                                          !originPlotID %in% block.outs,
                                          !treatment %in% outs,
                                          year %in% ins)
+#get rid of extra X columns
+abundance_df1 = subset(abundance_df1, select = -c(X,X.1))
+
+#save clean vertically-formatted abundance data
+write.csv(abundance_df1, file = "abundance_clean2018-2023.csv")
+
+###reformat to matrix or Vegan########
+
+#reformat data
+comm_matrix <- pivot_wider(abundance_df1, names_from = species, 
+                           values_from = occurrenceCount)
+
+#make matrix with plot IDs
+comm_matrix$ID = NA
+comm_matrix$ID <- paste(comm_matrix$turfID, "_",comm_matrix$year,"_",comm_matrix$treatmentOriginGroup)
+comm_matrixID <- comm_matrix %>% relocate(ID)
+comm_matrixID = subset(comm_matrixID, select = -c(turfID,originSite, destinationSite,
+                                                  originPlotID, 
+                                                  treatment,treatmentOriginGroup,year,
+                                                  date_yyyymmdd, functionalGroup,
+                                                  unknownMorpho, percentCover) )
+
+#replace NAs with 0s
+comm_matrixID[is.na(comm_matrixID)] <- 0
+
+#switch plot IDs to row names for the matrix including plot IDs
+comm_matrixID <- comm_matrixID %>%
+  group_by(ID) %>%
+  summarise_if(
+    is.numeric,
+    sum,
+    na.rm = FALSE
+  )
+comm_matrixID <- comm_matrixID %>% column_to_rownames(var = "ID")
+
+#save as csv 
+write.csv(comm_matrixID, file = "Data/Community_matrix_ByPlot.csv")
+
+##make abundance dataframe with only the three control treatments##############
+
+#use only control plots
+ins_controlonly <- c("netted_untouched","within_site_transplant","untouched" )
+
+#filter data for dataframe with control plots only
+abundance_df_controlonly <- abundance_df %>% filter(!is.na(treatment),
+                                                    !species %in% gc.outs,
+                                                    !originPlotID %in% block.outs,
+                                                    treatment %in% ins_controlonly,
+                                                    year %in% ins)
+
+###reformat data to matrix for vegan###############
+comm_matrix_controls <- pivot_wider(abundance_df_controlonly, names_from = species, 
+                                    values_from = occurrenceCount)
+
+#remove extra columns 
+comm_matrix_controls = subset(comm_matrix_controls, select = -c(originSite, 
+                                                                destinationSite,date_yyyymmdd, 
+                                                                functionalGroup, unknownMorpho, 
+                                                                percentCover) )
+
+#make matrix with plot IDs
+comm_matrix_controls$ID = NA
+
+comm_matrix_controls$ID <- paste(comm_matrix_controls$treatmentOriginGroup, "_",comm_matrix_controls$year, "_", comm_matrix_controls$originPlotID, "_", comm_matrix_controls$treatment)
+
+comm_matrix_controls <- comm_matrix_controls %>% relocate(ID)
+comm_matrix_controls = subset(comm_matrix_controls, select = -c(year, treatmentOriginGroup, X, X.1, treatment) )
+
+#replace NAs with 0s
+comm_matrix_controls[is.na(comm_matrix_controls)] <- 0
+
+#switch plot IDs to row names for the matrix including plot IDs
+comm_matrix_controls <- comm_matrix_controls %>%
+  group_by(ID) %>%
+  summarise_if(
+    is.numeric,
+    sum,
+    na.rm = TRUE
+  )
+comm_matrix_controls <- comm_matrix_controls %>% column_to_rownames(var = "ID")
+
+write.csv(comm_matrix_controls, file = "Data/Community_matrix_controlsonly.csv")
+
+
--- community_comp_change.R
+++ community_comp_change.R
@@ -1,95 +1,23 @@
 #data wrangling and reformatting with Lorah's transplant data
 #pivot wider to make comm phylo matrix 
 
-install.packages("lme4") #modelling
-install.packages("lmerTest") #provides p values
-install.packages("performance") #model diagnostics
-install.packages("ggeffects") #easy prediction & comparison
-install.packages("sjPlot") #visualize random effects
-install.packages("vegan") #community diversity
-install.packages("stringr") #to remove spaces
-
-library(tidyverse)
-library(lme4)
-library(lmerTest)
-library(performance)
-library(ggeffects)
-library(sjPlot)
-library(vegan)
-library(stringr)
-library(dplyr)
-
-#Data cleaning and formatting---------------------------------------------------
-#bring in data
-abundance_df <- read.csv("occurance2017-2023.csv")
-abundance_df$year <- as.factor(abundance_df$year)
-
-#use only these years
-ins <- c("2018","2019","2021","2022","2023")
-
-#get rid of extra control plots
-outs <- c("untouched","within_site_transplant")
-ins_controlonly <- c("netted_untouched","within_site_transplant","untouched" )
-
-# remove non-species from species column
-gc.outs <- c("litter", "bare_soil", "rock")
-
-#remove block 6 plots since they were transplanted in 2018
-block.outs <- c("mo6-1", "mo6-2", "mo6-3", "mo6-4","mo6-5", "pf6-1",
-                "pf6-2", "pf6-3","pf6-4", "um6-1", "um6-2","um6-3","um6-4",
-                "um6-5","um6-6")
-
-#filter data for things you never want
-abundance_df1 <- abundance_df %>% filter(!is.na(treatment),
-                                         !species %in% gc.outs,
-                                         !originPlotID %in% block.outs,
-                                         !treatment %in% outs,
-                                         year %in% ins)
-
+library(tidyverse) 
+library(lme4) #modelling
+library(lmerTest) #provides p values
+library(performance) #model diagnostics
+library(ggeffects) #easy prediction & comparison
+library(sjPlot) #visualize random effects
+library(vegan) #community diversity
+library(stringr) #to remove spaces
+library(dplyr) #data cleaning
 
 #Shannon diversity calculations------------------------------------------------
 
 ##Shannon for three different controls only, no transplant data#######
 
-#filter data for dataframe with control plots only
-abundance_df_controlonly <- abundance_df %>% filter(!is.na(treatment),
-                                                    !species %in% gc.outs,
-                                                    !originPlotID %in% block.outs,
-                                                    treatment %in% ins_controlonly,
-                                                    year %in% ins)
-
-#reformat data
-comm_matrix_controls <- pivot_wider(abundance_df_controlonly, names_from = species, 
-                           values_from = occurrenceCount)
-
-#remove extra columns 
-comm_matrix_controls = subset(comm_matrix_controls, select = -c(originSite, 
-                                              destinationSite,date_yyyymmdd, 
-                                              functionalGroup, unknownMorpho, 
-                                              percentCover) )
-
-#make matrix with plot IDs
-comm_matrix_controls$ID = NA
-
-comm_matrix_controls$ID <- paste(comm_matrix_controls$treatmentOriginGroup, "_",comm_matrix_controls$year, "_", comm_matrix_controls$originPlotID, "_", comm_matrix_controls$treatment)
-
-comm_matrix_controls <- comm_matrix_controls %>% relocate(ID)
-comm_matrix_controls = subset(comm_matrix_controls, select = -c(year, treatmentOriginGroup, X, X.1, treatment) )
-
-#replace NAs with 0s
-comm_matrix_controls[is.na(comm_matrix_controls)] <- 0
-
-#switch plot IDs to row names for the matrix including plot IDs
-comm_matrix_controls <- comm_matrix_controls %>%
-  group_by(ID) %>%
-  summarise_if(
-    is.numeric,
-    sum,
-    na.rm = TRUE
-  )
-comm_matrix_controls <- comm_matrix_controls %>% column_to_rownames(var = "ID")
-
 #calculate shannon diversity
+comm_matrix_controls <- read.csv("Data/Community_matrix_controlsonly.csv")
+
 shannon_controls <- diversity(comm_matrix_controls, index = "shannon")
 
 #make dataframe including plot numbers and reformat to use for plot
@@ -107,35 +35,8 @@ shannon_fig_controls <- ggplot(data = shannon_df_controls, aes(x=year, y=shannon
 
 plot(shannon_fig_controls)
 
-
 ##Shannon for each individual plot and averaging by origin site and treatment#####
-#reformat data
-comm_matrix <- pivot_wider(abundance_df1, names_from = species, 
-                           values_from = occurrenceCount)
-
-#make matrix with plot IDs
-comm_matrix$ID = NA
-comm_matrix$ID <- paste(comm_matrix$turfID, "_",comm_matrix$year,"_",comm_matrix$treatmentOriginGroup)
-comm_matrixID <- comm_matrix %>% relocate(ID)
-comm_matrixID = subset(comm_matrixID, select = -c(turfID,originSite, destinationSite,
-                                                  originPlotID, 
-                                                  treatment,treatmentOriginGroup,year,
-                                                  date_yyyymmdd, functionalGroup,
-                                                  unknownMorpho, percentCover,
-                                                  X, X.1) )
-
-#replace NAs with 0s
-comm_matrixID[is.na(comm_matrixID)] <- 0
-
-#switch plot IDs to row names for the matrix including plot IDs
-comm_matrixID <- comm_matrixID %>%
-  group_by(ID) %>%
-  summarise_if(
-    is.numeric,
-    sum,
-    na.rm = FALSE
-  )
-comm_matrixID <- comm_matrixID %>% column_to_rownames(var = "ID")
+comm_matrixID <- read.csv("Data/Community_matrix_ByPlot.csv")
 
 #calculate shannon diversity
 shannon_plots <- diversity(comm_matrixID, index = "shannon")
