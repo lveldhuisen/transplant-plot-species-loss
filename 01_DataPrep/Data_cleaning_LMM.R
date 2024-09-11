678cd2e4c82f61eaac7f13eea21547bdc64f7d0d
Leah Veldhuisen <77695333+lveldhuisen@users.noreply.github.com>
2024-09-11 09:34:27 -0700

    Update Data_cleaning_LMM.R

--- 01_DataPrep/Data_cleaning_LMM.R
+++ 01_DataPrep/Data_cleaning_LMM.R
@@ -49,6 +49,46 @@ pd_dat <- left_join(pd_df, join_dat, by = c("originPlotID", "year","treatmentOri
 pd_dat$replicates <- paste(pd_dat$originSite,"_", pd_dat$destinationSite,"_",
                           pd_dat$treatment,"_", pd_dat$year)
 
+##MPD#########
+
+#bring in data
+mpd_df <- read.csv("Data/MPD_byPlot.csv")
+
+#rename columns to match abundance dataset
+mpd_df <- mpd_df %>% 
+  rename(
+    treatmentOriginGroup = tx_site,
+    originPlotID = plotID)
+
+#get rid of merged column
+mpd_df = subset(mpd_df, select = -c(X))
+
+#merge dataframes
+mpd_dat <- left_join(mpd_df, join_dat, by = c("originPlotID", "year","treatmentOriginGroup"))
+
+#add column to ID replication
+mpd_dat$replicates <- paste(mpd_dat$originSite,"_", mpd_dat$destinationSite,"_",
+                           mpd_dat$treatment,"_", mpd_dat$year)
+
+##MNTD######
+#bring in data
+mntd_df <- read.csv("Data/MNTD_byPlot.csv")
+
+#rename columns to match abundance dataset
+mntd_df <- mntd_df %>% 
+  rename(
+    treatmentOriginGroup = tx_site,
+    originPlotID = plotID)
+
+#get rid of merged column
+mntd_df = subset(mntd_df, select = -c(X))
+
+#merge dataframes
+mntd_dat <- left_join(mntd_df, join_dat, by = c("originPlotID", "year","treatmentOriginGroup"))
+
+#add column to ID replication
+mntd_dat$replicates <- paste(mntd_dat$originSite,"_", mntd_dat$destinationSite,"_",
+                            mntd_dat$treatment,"_", mntd_dat$year)
 
 
 
