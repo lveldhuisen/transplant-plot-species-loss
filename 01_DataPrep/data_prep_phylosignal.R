0e9daf0ba3dead7e9878251a1b626b300a522a10
Leah Veldhuisen <77695333+lveldhuisen@users.noreply.github.com>
2024-11-19 14:03:27 -0700

    Create data_prep_phylosignal.R

---
+++ 01_DataPrep/data_prep_phylosignal.R
@@ -0,0 +1,21 @@
+library(tidyverse)
+library(dplyr)
+library(picante)
+library(geiger)
+library(ape)
+library(vegan)
+library(forcats)
+library(broom)
+library(janitor)
+library(patchwork)
+library(car)
+
+#import S&B phylogeny--------------------
+
+#bring in .tre file
+setwd("~/Library/CloudStorage/OneDrive-UniversityofArizona/Arizona PhD/Research/RMBL phylogeny/Smith&Brown18")
+SBtree <- read.tree(file = "ALLMB.tre")
+write.tree(SBtree)
+is.rooted(SBtree)
+
+#import community data matrix
