b2c5dde3ab105a1584f174f1e7719f2e4acdeca0
Leah Veldhuisen <77695333+lveldhuisen@users.noreply.github.com>
2024-11-20 15:16:09 -0700

    Update Species_abundance_heatmap.R

--- 03_Figures/Species_abundance_heatmap.R
+++ 03_Figures/Species_abundance_heatmap.R
@@ -3,14 +3,16 @@ library(tidyverse)
 
 #data cleaning--------------
 #bring in data
-species_df <- read.csv("Data/abundance_clean2018-2023.csv")
+abundance_df1 <- read.csv("Data/abundance_clean2018-2023.csv")
 
 #remove years other than 2018 and 2023
 year.outs <- c("2019", "2021", "2022")
-species_df <- species_df %>% filter(!year %in% year.outs)
+tx.outs <- c("netted_untouched","untouched")
+abundance_df1 <- abundance_df1 %>% filter(!year %in% year.outs,
+                                    !treatment %in% tx.outs)
 
 #remove extra columns
-species_df = subset(species_df, select = -c(X,turfID,originSite,
+abundance_df1 = subset(abundance_df1, select = -c(X,turfID,originSite,
                                             destinationSite,
                                             originPlotID,
                                             date_yyyymmdd,
@@ -18,10 +20,11 @@ species_df = subset(species_df, select = -c(X,turfID,originSite,
                                             GBIF_citation,
                                             unknownMorpho,
                                             percentCover,
-                                            Origin))
+                                            Origin, 
+                                            treatmentOriginGroup))
 
 #collapse all species records by treatment and add abundance across plots
-species_df <- species_df %>%
+species_df <- abundance_df1 %>%
   group_by(species, year, treatment) %>%
   summarise_if(
     is.numeric,
@@ -29,10 +32,21 @@ species_df <- species_df %>%
     na.rm = TRUE
   )
 
-#add change column
-species_df$abundance_change = NA
+#create new column for 2018 only
+species_df18 <- species_df %>% mutate(occ_2018 = ifelse(year == "2018", occurrenceCount, NA))
 
-#add change values
+species_df18 = subset(species_df18, select = -c(occurrenceCount, year))
+
+#create new column for 2023 only
+species_df23 <- species_df %>% mutate(occ_2023 = ifelse(year == "2023", occurrenceCount, NA))
+
+species_df23 = subset(species_df23, select = -c(occurrenceCount, year))
+
+##collapse 2018 and 2023 abundances into correct columns
+test <- species_df18 %>% left_join(species_df23, by = c("species", "treatment"),relationship = "many-to-many")
+
+
+
 
 
 #figure----------
