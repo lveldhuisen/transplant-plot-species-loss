bf2827957355e98f1e2ceab13834fd6fd216eeae
Leah Veldhuisen <leah.veldhuisen@gmail.com>
2025-02-26 10:43:11 -0700

    update

--- 01_DataPrep/Data_cleaning_community_diversity.R
+++ 01_DataPrep/Data_cleaning_community_diversity.R
@@ -132,3 +132,6 @@ comm_matrix_controls <- comm_matrix_controls %>% column_to_rownames(var = "ID")
 
 write.csv(comm_matrix_controls, file = "Data/Community_matrix_controlsonly.csv")
 
+#Calculate how species lost and gained from 2017-2023 for each plot-------------
+
+
